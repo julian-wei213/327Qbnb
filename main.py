e85a8635aee2e0bc027f35c39a8ede734cf94e81
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2022-09-22 19:26:57 -0400

    Update Listing Class main.py
    Added get functions for attributes

--- main.py
+++ main.py
@@ -9,7 +9,18 @@ db = SQLAlchemy(app)
 
 class Listing(db.Model):
     id = db.Column(db.Integer, primary_key=true)
-    adress = db.Column(db.String)
+    address = db.Column(db.String)
     price = db.Column(db.Integer)
     owner = db.Column(db.Integer)
-    
+
+    def __repr__(self):
+        return '<Listing %r>' % self.id
+
+    def get_address(self):
+        return '<Listing %r>' % self.address
+
+    def get_price(self):
+        return '<Listing %r>' % self.price
+
+    def get_owner(self):
+        return '<Listing %r>' % self.owner
