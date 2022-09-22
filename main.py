15219bb2ee81768edba1d20e6b247c33e0176dbd
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2022-09-22 15:23:19 -0400

    Update main.py
    Added Listing Class and Some Attributes

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
+
+
+class Listing(db.Model):
+    id = db.Column(db.Integer, primary_key=true)
+    adress = db.Column(db.String)
+    price = db.Column(db.Integer)
+    owner = db.Column(db.Integer)
+    
