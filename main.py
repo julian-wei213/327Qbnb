6c1841d8ccbeb12ee0484b3cffab27beb87882a0
Ensaurus <18ehm1@queensu.ca>
2022-09-22 22:44:08 -0400

    Implemented Transaction

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
+
+
+class Transaction(db.Model):
+    """Data model for Transactions
+    Attributes
+    ----------
+    id : Integer
+        represents a unique id
+    listing_id : Integer
+        represents the associated listing in database
+    user_id : Integer
+        represents the user in database that booked the listing
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    listing_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        """Returns the string representation of Transaction"""
+        return '<%r booking %r>' % \
+            Listing.query.filter_by(id=self.listing_id), \
+            User.query.filter_by(id=self.user_id)
