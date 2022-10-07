096c62d40625d9fd4a4dd51d732ad74744bb7677
julian-wei213 <julianwei@hotmail.ca>
2022-10-07 11:32:21 -0400

    update listing function and testing

--- qbay/models.py
+++ qbay/models.py
@@ -1,5 +1,7 @@
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
+import re
+from datetime import date
 
 
 '''
@@ -19,6 +21,7 @@ class User(db.Model):
         db.String(120), unique=True, nullable=False)
     password = db.Column(
         db.String(120), nullable=False)
+    listing = db.relationship("Listing", backref="user")
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -54,6 +57,146 @@ class Listing(db.Model):
 
 # create all tables
 db.create_all()
+
+
+def create_listing(title: str, description: str, price: float,
+                   last_modified_date: date, owner_id: int):
+    '''
+    Creates a listing
+      Attributes:
+        title (str):               listing title
+        description (str):         listing description
+        price (float):             listing price
+        last_modified_date (date): last modified date of listing
+        owner_id (int):            listing owner's id
+      Returns:
+        The listing object if succeeded otherwise None
+    '''
+    # Satisfy R4-1
+    if title == '':
+        return None
+
+    if title[0] == ' ' or title[-1] == ' ':
+        return None
+
+    if not title.replace(' ', '').isalnum():
+        return None
+
+    # Satisfy R4-2
+    if len(title) > 80:
+        return None
+
+    # Satisfy R4-3
+    if len(description) < 20 or len(description) > 2000:
+        return None
+
+    # Satisfy R4-4
+    if len(description) <= len(title):
+        return None
+
+    # Satisfy R4-5
+    if price < 10 or price > 10000:
+        return None
+
+    # Satisfy R4-6
+    if last_modified_date <= date(2021, 1, 2) or \
+       last_modified_date >= date(2025, 1, 2):
+        return None
+
+    # Satisfy R4-7
+    user = User.query.filter_by(id=owner_id).first()
+    if user is None:
+        return None
+
+    if user.email == '':
+        return None
+
+    # Satisfy R4-8
+    if len(Listing.query.filter_by(title=title).all()) > 0:
+        return None
+
+    # create a new listing
+    listing = Listing(title=title, description=description,
+                      price=price, last_modified_date=last_modified_date,
+                      owner_id=owner_id)
+
+    # add it to the current database session
+    db.session.add(listing)
+    # actually save the user object
+    db.session.commit()
+
+    return listing
+
+
+def update_listing(listing, title=None, description=None, price=None):
+    if title is None and description is None and price is None:
+        return None
+
+    if not isinstance(listing, Listing):
+        return listing
+
+    if title is not None:
+        if not isinstance(title, str):
+            return None
+
+        if title == "":
+            return None
+
+        if len(title) > 80:
+            return None
+
+        # TODO Title alphanumeric no space on prefix and suffix
+
+        check = re.match("^(?! )[A-Za-z0-9 ]*(?<! )$", title)
+
+        if check is None:
+            return None
+
+        # TODO Check if title has been used
+
+        existed = Listing.query.filter_by(title=title).all()
+        if len(existed) > 0:
+            return None
+
+        listing.title = title
+
+    if description is not None:
+        if not isinstance(description, str):
+            return None
+
+        if len(description) < 20 or len(description) > 2000:
+            return None
+
+        if title is not None:
+            if len(description) < len(title):
+                return None
+        else:
+            if len(description) < len(listing.title):
+                return None
+
+        listing.description = description
+
+    if price is not None:
+        if not isinstance(price, float):
+            return None
+
+        if price < 10 or price > 10000:
+            return None
+
+        if price < listing.price:
+            return None
+
+        listing.price = price
+
+    if date.today() > date(2021, 1, 2) and \
+       date.today() < date(2025, 1, 2):
+        listing.last_modified_date = date.today()
+    else:
+        return None
+
+    db.session.commit()
+
+    return listing
 
 
 def register(name, email, password):
--- qbay_test/test_models.py
+++ qbay_test/test_models.py
@@ -1,4 +1,6 @@
-from qbay.models import register, login
+from qbay.models import create_listing, register, login
+from qbay.models import update_listing, User, Listing
+from datetime import date
 
 
 def test_r1_7_user_register():
@@ -13,9 +15,9 @@ def test_r1_7_user_register():
 
 def test_r2_1_login():
     '''
-    Testing R2-1: A user can log in using her/his email address 
+    Testing R2-1: A user can log in using her/his email address
       and the password.
-    (will be tested after the previous test, so we already have u0, 
+    (will be tested after the previous test, so we already have u0,
       u1 in database)
     '''
 
@@ -25,3 +27,179 @@ def test_r2_1_login():
 
     user = login('test0@test.com', 1234567)
     assert user is None
+
+
+def test_r4_1_create_listing():
+    '''
+    Testing R4-1: The title of the product has to be alphanumeric-only, and
+      space allowed only if it is not as prefix and suffix.
+    '''
+    user = User.query.filter_by().first()
+
+    # Case 1: Empty Title
+    listing = create_listing('', 'description of listing',
+                             30.00, date(2022, 10, 6), user.id)
+    assert listing is None
+
+    # Case 2: Regular Title
+    listing = create_listing('The Title', 'description of listing',
+                             30.00, date(2022, 10, 6), user.id)
+    assert listing is not None
+
+    # Case 3: Regular Title with Numbers
+    listing = create_listing('The Title62', 'description of listing',
+                             30.00, date(2022, 10, 6), user.id)
+    assert listing is not None
+
+    # Case 4: Title with Space as Prefix
+    listing = create_listing(' The Title62', 'description of listing',
+                             30.00, date(2022, 10, 6), user.id)
+    assert listing is None
+
+    # Case 5: Title with Space as Suffix
+    listing = create_listing('The Title62 ', 'description of listing',
+                             30.00, date(2022, 10, 6), user.id)
+    assert listing is None
+
+    # Case 6: Title with Underscore
+    listing = create_listing('The Tit_le', 'description of listing',
+                             30.00, date(2022, 10, 6), user.id)
+    assert listing is None
+
+
+def test_r5_1_update_listing():
+    listing = Listing.query.filter_by().first()
+
+    # Test update title
+    ntitle = "new title"
+    assert (ntitle == listing.title) is False
+
+    update_listing(listing, title=str(ntitle))
+    assert (ntitle == listing.title) is True
+
+    # Test update description
+    long_description = "This is a detailed description with no grammar"
+    assert (long_description == listing.description) is False
+
+    update_listing(listing, description=str(long_description))
+    assert (long_description == listing.description) is True
+
+    # Test update price
+    f = float(21000)
+    assert update_listing(listing, price=f) is None
+    f = float(2)
+    assert update_listing(listing, price=f) is None
+
+    f = float(5000)
+    assert update_listing(listing, price=f) is not None
+    assert (float(5000) == listing.price) is True
+
+    # Test multiple
+    assert update_listing(listing, title="Three together",
+                          description="There will be a total of three changes",
+                          price=float(6000)) is not None
+
+    assert (listing.title == "Three together") is True
+    assert (listing.description == "There will be a total of three changes")\
+        is True
+    assert (listing.price == 6000) is True
+
+    assert update_listing(listing, title="Three together",
+                          description="There will be a total of three changes",
+                          price=float(500)) is None
+
+
+def test_r5_2_update_listing():
+    listing = Listing.query.filter_by().first()
+
+    assert update_listing(listing, price=float(20)) is None
+    assert (listing.price == 20) is False
+    assert update_listing(listing, price=float(6200)) is not None
+    assert (listing.price == 6200) is True
+
+
+def test_r5_3_update_listing():
+    listing = Listing.query.filter_by().first()
+
+    update_listing(listing, title=str("title2"))
+    assert (listing.last_modified_date == date.today()) is True
+
+
+def test_r5_4_update_listing():
+    listing = Listing.query.filter_by().first()
+
+    # Title not alphanumeric
+    assert update_listing(listing, title="%",
+                          description="new stuff was written in here") is None
+
+    # Space as prefix in title
+    assert update_listing(listing, title=" title2",
+                          description="new stuff was written in here") is None
+
+    # Space as suffix in title
+    assert update_listing(listing, title="title2 ",
+                          description="new stuff was written in here") is None
+
+    # Space as prefix and suffix in title
+    assert update_listing(listing, title=" title2 ",
+                          description="new stuff was written in here") is None
+
+    # Space in middle of title
+    assert update_listing(listing, title="ti tle2",
+                          description="new stuff was written in") is not None
+
+    # No Spaces
+    assert update_listing(listing, title="title3",
+                          description="new stuff was written in") is not None
+
+    # Title less than 80 characters
+
+    # 79 characters
+    assert update_listing(listing, title='x' * 80) is not None
+
+    # 80 characters
+    assert update_listing(listing, title='x' * 81) is None
+
+    # 1 character
+    assert update_listing(listing, title='x' * 5) is not None
+
+    # Description no less than 20 characters and no greater than 2000
+    # characters
+
+    # 20 characters
+    assert update_listing(listing, description='x' * 20) is not None
+
+    # 19 characters
+    assert update_listing(listing, description='x' * 19) is None
+
+    # 2000 characters
+    assert update_listing(listing, description='x' * 2000) is not None
+
+    # 2001 characters
+    assert update_listing(listing, description='x' * 2001) is None
+
+    # Arbitrary numbers
+    assert update_listing(listing, description='@*!&@ !@!+"{> :>>~ |":>DA>D')\
+           is not None
+
+    # Description must not be smaller than title
+
+    # Smaller description than title
+    short_description = "smo"
+    assert update_listing(listing, description=short_description) is None
+
+    # Larger description than title
+    short_description = "This is a longer description then the other one"
+    assert update_listing(listing, description=short_description) is not None
+
+    # Price needs to in range [10, 10000], already did in r5-1
+
+    # There can't be two listings with same title
+
+    update_listing(listing, title="SAMETITLE")
+
+    listing2 = Listing.query.filter_by(title="The Title62").first()
+
+    assert update_listing(listing2, title="SAMETITLE") is None
+
+    assert update_listing(listing2, title="Thishere") is not None
