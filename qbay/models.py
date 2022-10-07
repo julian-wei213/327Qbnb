1c28fc2b760aadc9c34012549056061ff2b2aa1b
Brian Cho <b.cho@queensu.ca>
2022-10-06 22:03:06 -0400

    Add user id attribute
    Needed to pass test

--- qbay/models.py
+++ qbay/models.py
@@ -11,11 +11,12 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+    id = db.Column(
+        db.Integer, primary_key=True)
     username = db.Column(
         db.String(80), nullable=False)
     email = db.Column(
-        db.String(120), unique=True, nullable=False,
-        primary_key=True)
+        db.String(120), unique=True, nullable=False)
     password = db.Column(
         db.String(120), nullable=False)
 
